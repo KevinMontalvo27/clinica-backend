34b436e7ba0621d514f78200b1b8e41765c9ef9c
KevinMontalvo27 <montalvokevin375@gmail.com>
2025-11-05 01:11:52 -0700

    feat: update schedule-exception entity

--- src/appointments/entities/schedule-exception.entity.ts
+++ src/appointments/entities/schedule-exception.entity.ts
@@ -7,4 +7,19 @@ export class ScheduleException extends BaseEntityCustom {
     @ManyToOne(() => Doctor, doctor => doctor.scheduleExceptions)
     @JoinColumn({ name: 'doctorId' })
     doctor: Doctor;
+
+    @Column()
+    doctorId: string;
+
+    @Column({ type: 'date' })
+    exceptionDate: Date;
+
+    @Column({ type: 'time', nullable: true })
+    startTime: string | null;
+
+    @Column({ type: 'time', nullable: true })
+    endTime: string | null;
+
+    @Column({ type: 'text', nullable: true })
+    reason: string;
 }
